cf34e45bd83cff699c11cb91e969a65ff0f40da7
DinhNguyenNgocMinh <podnponm@gmail.com>
2026-07-12 15:58:08 +0700

    Prepare backend for Render

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ const pool = require("./db");
 const authRoutes = require("./routes/auth");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.get("/", async (req, res) => {
@@ -24,6 +24,8 @@ app.get("/", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
